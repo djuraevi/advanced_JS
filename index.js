d54f22d5a93def41672ef3f6d87deab712732e15
djuraevi <djuraevi@yandex.ru>
2022-08-19 16:13:07 +0300

    homework lesson 4

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 const BASE = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 const GOODS = '/catalogData.json';
 
-function service (url) {
-  return new Promise((resolve) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      const loadHandler = () => {
-        resolve(JSON.parse(xhr.response))
-      }
-      xhr.onload = loadHandler;
-      xhr.send(); 
-  })
-}
+const service = (url) => fetch(url)
+  .then((res) => res.json());
+
+// function service (url) {
+//   return new Promise((resolve) => {
+//       const xhr = new XMLHttpRequest();
+//       xhr.open('GET', url);
+//       const loadHandler = () => {
+//         resolve(JSON.parse(xhr.response))
+//       }
+//       xhr.onload = loadHandler;
+//       xhr.send();
+//   })
+// }
 
 
 class GoodsItem {
@@ -63,3 +66,45 @@ goodsList.fetchGoods().then(() => {
   goodsList.render();
 });
 goodsList.sumProducts();
+
+
+// 1) Какие виды областей видимости вы знаете? Написать ответ ниже
+
+// глобальная - обьявление переменных вне функций и модулей, доступ к откуда угодно внутри файла
+// локальная - переменные используемые в теле функции, и доступные только внутри нее
+// внутри блока - локальная переменная для функции, скрыта внутри видимости блока и доступна внутри этого блока
+// замыкание - это способность функции запоминать область видимости и обращаться к ней из любого места приложения
+
+
+// 2) Исправьте код так чтобы в консоль выводились числа от 0 до 10
+for (let i = 0; i <= 10; i++) {
+   setTimeout(() => {
+      console.log(i);
+   }, 0)
+}
+
+
+// 3) Исправьте код так чтобы в консоль выводилось "John"
+var firstName = "Elena"
+const obj = {
+   firstName: 'John',
+   sayFirstName: function f(){
+      console.log(this.firstName)
+   }
+}
+obj.sayFirstName();
+
+
+// 4) Исправьте код так чтобы в консоль не выводилась ошибка (нельзя исправлять тело функции getArrowFunction)
+ const user = {
+   age: 20
+}
+function getArrowFunction() {
+   "use strict"
+   return () => {
+      console.log(this.age)
+   }
+}
+
+const arrowFunction = getArrowFunction.call(user);
+arrowFunction();
